f9081d915c83f8250a3cf776712eca1b8cb50dc8
Jessica Fransson <jessicafransson@outlook.com>
2022-10-13 13:17:24 +0000

    updates extensions testing

--- student.py
+++ student.py
@@ -1,7 +1,17 @@
 from datetime import date, timedelta
 
+
 class Student:
-    "a student class as base for method testing"
+    """ 
+    A Student class as a basis for method testing 
+    * Create a new test method called test_apply_extension
+    * Inside test_apply_extension, store the current end_date for our student_instance in a variable called old_end_date
+    * Call a method named apply_extension that will take a number of days as an argument on the student instance to update the end_date
+    * Assert whether the instance's end_date equals the old end date plus the days argument that was passed in using timedelta
+    * In the student class, create a new method called apply_extension that has a parameter called days
+    * Use the timedelta method from datetime to update the end_date property
+    * Run the tests to confirm they are working 
+    """
 
     def __init__(self, first_name, last_name):
         self._first_name = first_name
@@ -18,7 +28,8 @@ class Student:
     def email(self):
         return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
 
-
     def alert_santa(self):
         self.naughty_list = True
 
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
