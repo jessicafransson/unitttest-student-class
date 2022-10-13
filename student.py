b75fd2de0261f741d49a1f1cf266441d4effbe05
Jessica Fransson <jessicafransson@outlook.com>
2022-10-13 14:06:52 +0000

    added test for schedule course

--- student.py
+++ student.py
@@ -1,17 +1,9 @@
 from datetime import date, timedelta
+import requests
 
 
 class Student:
-    """ 
-    A Student class as a basis for method testing 
-    * Create a new test method called test_apply_extension
-    * Inside test_apply_extension, store the current end_date for our student_instance in a variable called old_end_date
-    * Call a method named apply_extension that will take a number of days as an argument on the student instance to update the end_date
-    * Assert whether the instance's end_date equals the old end date plus the days argument that was passed in using timedelta
-    * In the student class, create a new method called apply_extension that has a parameter called days
-    * Use the timedelta method from datetime to update the end_date property
-    * Run the tests to confirm they are working 
-    """
+    """ A Student class as a basis for method testing """
 
     def __init__(self, first_name, last_name):
         self._first_name = first_name
@@ -33,3 +25,12 @@ class Student:
 
     def apply_extension(self, days):
         self.end_date += timedelta(days=days)
+
+    def course_schedule(self):
+        response = requests.get(
+            f"https://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong"
